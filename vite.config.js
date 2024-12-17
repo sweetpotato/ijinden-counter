36ff2cfb7a645fd3d478728597d1e66ddfc75b66
sweetpotato <sweetpotato@users.noreply.github.com>
2024-12-17 22:54:06 +0900

    config: ベースパスを相対 URL から絶対 URL に修正
    今後の OGP 対応のため。

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ijinden-counter/',
+  base: 'https://sweetpotato.github.io/ijinden-counter/',
   build: {
     outDir: 'docs',
   },
